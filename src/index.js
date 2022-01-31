22c00b88cbfc0300143d66405d314924f115ea7f
setiawankarnata <setiawankarnata@gmail.com>
2022-01-31 19:23:10 +0700

    First Init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import foods from "./foods";
+import {choice, remove} from "./helpers";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Randomly draw a fruit
+let fruit = choice(foods);
+// Log the message "I'd like one RANDOMFRUIT, please."
+console.log(`I'd like one ${fruit}, please.`);
+// Log the message "Here you go: RANDOMFRUIT"
+console.log(`Here you go: ${fruit}`);
+// Log the message "Delicious! May i have another?"
+console.log('Delicious! May I have another?');
+// Remove the fruit from the array of fruits
+let remaining = remove(foods, fruit);
+// Log the message "I'm sorry, we're all out. We have FRUITSLEFT left."
+console.log(`I'm sorry, we're all out. We have ${remaining.length} left.`);
